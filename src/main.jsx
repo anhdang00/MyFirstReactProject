361dae032f9c5b91faa27c30b6b3a79a2836e763
anhdang00 <adang7@hawk.iit.edu>
2024-08-07 08:34:57 -0500

    add paths

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {path:'/', element:<App></App>},
+  {path:'/lobby', element:<div>Lobby</div>},
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
+    
   </React.StrictMode>,
 )

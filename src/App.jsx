2071ed79b8334a91746bb9c23cc481d232f02cd2
anhdang00 <adang7@hawk.iit.edu>
2024-08-06 22:34:31 -0500

    create function eachOne()

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function eachOne(){
+  
+}
+
 function App() {
  const TIC_TAC_TOE= new Array(3).fill(new Array(3).fill(null))
  

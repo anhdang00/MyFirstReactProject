6c98590c63e5be312117d8bc1211b5fa9c24de54
anhdang00 <adang7@hawk.iit.edu>
2024-08-07 08:48:35 -0500

    change paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom'
+
+const Lobby=()=>{
+  const navigate = useNavigate()
+
+  return <div>Lobby <button onClick={()=>{
+    navigate('/newSession')
+  }}>Take me to the game</button></div>
+}
 
 const router = createBrowserRouter([
-  {path:'/', element:<App></App>},
-  {path:'/lobby', element:<div>Lobby</div>},
+  {path:'/:sessionId', element:<App></App>},
+  {path:'/lobby', element:<Lobby></Lobby>},
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
